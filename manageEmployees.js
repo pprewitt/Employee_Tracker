64105be2ed6fced7a670c01c5a91ab1cf62b452b
Dr Phil <philip.prewitt@gmail.com>
2020-09-11 12:53:33 -0600

    initial inquirer choice and switch case functions added

--- manageEmployees.js
+++ manageEmployees.js
@@ -10,12 +10,67 @@ var connection = mysql.createConnection({
   database: "employees_DB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
   showLogo()
-  // beginQuestions()
+  beginQuestions()
 })
+
+const beginQuestions = () => {
+  return inquirer.prompt({
+    name: "userSelection",
+    type: "list",
+    message: "What would you like to do today?",
+    choices: [
+      "View All Employees",
+      "View All Departments",
+      "View All Roles",
+      "Add a New Employee",
+      "Add a New Department",
+      "Add a New Role",
+      "Update an Employee Role",
+      "End Application"
+    ]
+  }).then(answer => {
+  switch (answer.userSelection) {
+    case ("View All Employees"):
+      viewAllEmployees();
+      break;
+    case ("View All Departments"):
+      viewAllDepartments();
+      break;
+    case ("View All Roles"):
+      viewAllRoles();
+      break;
+    case ("Add a New Employee"):
+      addEmployee();
+      break;
+    case ("Add a New Department"):
+      addDepartment();
+      break;
+    case ("Add a New Role"):
+      addRole();
+      break;
+    case ("View All Employees by Department"):
+      viewAllEmployeesByDepartment();
+      break;
+    case ("Update an Employee Role"):
+      updateRole();
+      break;
+    case ("End Application"):
+      console.log("Goodbye!");
+      showLogo();
+      connection.end();
+      break;
+    default:
+      console.log("An error Occurred");
+      connection.end();
+  }
+});
+}
+
+
 
 const showLogo = () => {
   console.log('8888888888                        888                                     ');
